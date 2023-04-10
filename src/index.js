bab675603ff31b8ff178448deb82ec4c42de967f
Afek-Sakaju <100536372+Afek-Sakaju@users.noreply.github.com>
2023-04-10 18:04:13 +0300

    .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-// export { ManagedColorsTable } from "./components";
-// export { randomizeColorsFromList } from "./utils";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+export { ManagedColorsTable } from "./components";
+export { randomizeColorsFromList } from "./utils";
